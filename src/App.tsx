de293284e0c75e9e6981e5bc80809a873bf7afa8
strom99 <alissoncc17@gmail.com>
2025-02-01 01:38:47 +0100

    selecionar imagen

--- src/App.tsx
+++ src/App.tsx
@@ -7,42 +7,49 @@ import rata from './assets/rata.jpg';
 
 interface ImgViewProps {
   data: any,
-  select: any,
+  select: any
 }
 
 
-const ImgView: React.FC<ImgViewProps> = ({ data, select }) => {
-
+const ImgView: React.FC<ImgViewProps> = ({ data, select}) => {
+  const [blue,setBlue] = useState(false);
   if (!data) return null; // Retorna nada si no hay data
+  const dynamicStyle = {
+    filter: blue ? "drop-shadow(1px 8px 26px #ff2fff)" : "",
+  };
+  console.log(blue)
 
   return (
-    <div  className='img'>
-      <img onClick={select} src={URL.createObjectURL(data)} alt={data.name} />
+    <div style={dynamicStyle} className='img'>
+      <img onClick={(e) =>{select(),setBlue(!blue)}} src={URL.createObjectURL(data)} alt={data.name} />
       <span>{data.name}</span>
     </div>
   )
 }
 
 
-ImgView.prototype = {
-  data: PropTypes.string, // Esperamos que `data` sea una cadena (URL de la imagen)
-};
+// ImgView.prototype = {
+//   data: PropTypes.string, // Esperamos que `data` sea una cadena (URL de la imagen)
+// };
 
 
 export default function App() {
   const [error, setError] = useState('');
   const [selectedImages, setSelectedImages] = useState<File[]>([]);
-  const [renam, setRenam] = useState([]);
+  const renam:any = [];
+  const [validImages, setValidImages] = useState<File[]>([]);
+
+
 
   const handleFileChange = (e:any) => {
     const files = e.target.files; // Obtenemos el primer archivo
-    const validImages = [];
     const invalidImages = [];
+    const valid = [];
 
     // Iteramos por cada archivo para validarlo
     for (let file of files) {
       if (file.type === 'image/png' || file.type === 'image/jpeg') {
-        validImages.push(file); // Generamos la URL para imágenes válidas
+        valid.push(file);
       } else {
         invalidImages.push(file.name);
       }
@@ -54,13 +61,14 @@ export default function App() {
       setError('');
     }
 
-    // Actualizamos el estado con las imágenes válidas
-    setSelectedImages(validImages);
+    setValidImages(valid);
 
   };
 
   function onClicked(image:File){
-    console.log("hello")
+    renam.push(image);
+    console.log('renam'+ renam)
+
   }
 
 
@@ -75,9 +83,9 @@ export default function App() {
       {error && <p style={{ color: 'red' }}>{error}</p>}
     </div>
       <div className='boxImg'>
-      {selectedImages.length > 0 ? (
-          selectedImages.map((image, index) => (
-            <ImgView key={index} data={image} select={()=>onClicked(image)} />
+      {validImages.length > 0 ? (
+          validImages.map((image, index) => (
+            <ImgView key={index} data={image} select={()=>onClicked(image)}/>
           ))
         ) : (
           <p>No hay imágenes seleccionadas.</p>
